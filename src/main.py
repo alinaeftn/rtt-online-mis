19c5fb3ab4cce20fb12755a9261f34de8c373af9
filiuta <filiuta@adobe.com>
2023-11-29 13:40:01 +0200

    working version

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-from graph_onedrive import OneDrive
-
 from graph_onedrive import OneDriveManager
 
 # Use a context manager to manage the session
@@ -8,5 +6,7 @@ with OneDriveManager(config_path="config.json", config_key="onedrive") as my_dri
     my_drive.get_usage(verbose=True)
 
     # Upload a file to the root directory
-    new_file_id = my_drive.upload_file("my-photo.jpg", verbose=True)
+    new_file_id = my_drive.upload_file("my-file.txt", verbose=True)
+    new_folder_id = my_drive.make_folder("my-folder")
+    my_drive.move_item(new_file_id, new_folder_id)
 
